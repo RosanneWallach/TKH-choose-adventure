d2507c2d4f8255a60c68e5315829c9871e256ac5
Rosanne Wallach <Rosanne.wallach@bryanuniversity.edu>
2022-10-05 18:26:12 -0400

    updated changes

--- adventure.js
+++ adventure.js
@@ -22,14 +22,14 @@ console.log(userName);
 
 let newMessage = prompt("How are you doing today? type 'good' or 'not good' ")
 
-if (newMessage = prompt === 'good'){
+if (newMessage === 'good'){
     alert('Thats great! Are you ready to go out?')
     let activity = prompt("type 'yes' or 'no' ")
     if(activity === 'yes'){
-
+        alert("Have a good time out")
     }
     else if('no'){
-
+        alert("See you there!")
     }
     else {
         alert("error")
@@ -38,7 +38,17 @@ if (newMessage = prompt === 'good'){
 }
 
 else if(newMessage === 'not good'){
-
+    alert('Thats sad! Are you ok?')
+    let activity = prompt("type 'yes' or 'no' ")
+    if(activity === 'yes'){
+        alert("Have a good time out")
+    }
+    else if('no'){
+        alert("See you there!")
+    }
+    else {
+        alert("error")
+    }
 }
 
 else{
@@ -48,13 +58,14 @@ else{
 let place = prompt("type 'city' or 'country' ")
 
 if(place === 'city'){
-    alert('you are going to the city')
+    city = prompt("What city you're visiting?")
+    alert('you are going to ' + city)
     let activity = prompt("type 'museum' or 'shopping' ")
     if(activity === 'museum'){
-
+        alert("Have fun at the museum!")
     }
     else if('shopping'){
-
+        alert("Happy Shopping, Sad wallet!")
     }
     else {
         alert("error")
@@ -62,7 +73,9 @@ if(place === 'city'){
 }
 
 else if(place === 'country'){
-
+    country = prompt("What city you're visiting?")
+    purpose = prompt("What is the purpose of your visit?")
+    alert('you are going to ' + country + " For " + purpose)
 }
 
 else{
